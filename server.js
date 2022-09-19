d101dd77603ccc7b0157fe85d035779dc38fd284
Angel Ivanov Mac <angel.iwanov@gmail.com>
2022-09-19 16:55:12 +0300

    fix

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile')
 const image = require('./controllers/image')
 
-// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
     client: 'pg',
@@ -15,7 +15,7 @@ const db = knex({
       connectionString : process.env.DATABASE_URL,
       ssl : true,
     }
-  }); 
+}); 
 
 const app = express();
 app.use(express.json());
